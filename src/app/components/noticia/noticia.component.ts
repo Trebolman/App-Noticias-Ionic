eb0370c25c4699f673bf5552de68b9a0c91c18e5
trebolman@gmail.com <trebolman@gmail.com>
2020-10-25 17:45:30 -0500

    Boton action sheet success!

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,21 +1,57 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { Article } from 'src/app/interfaces/interfaces';
+import { Component, Input, OnInit } from "@angular/core";
+import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
+import { ActionSheetController } from "@ionic/angular";
+import { Article } from "src/app/interfaces/interfaces";
 
 @Component({
-  selector: 'app-noticia',
-  templateUrl: './noticia.component.html',
-  styleUrls: ['./noticia.component.scss'],
+  selector: "app-noticia",
+  templateUrl: "./noticia.component.html",
+  styleUrls: ["./noticia.component.scss"],
 })
 export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() index: number;
-  constructor(private iab: InAppBrowser) { }
+  constructor(
+    private iab: InAppBrowser,
+    private actionSheetCtrl: ActionSheetController
+  ) {}
 
-  ngOnInit() {}
-
-  onClick(){
-    this.iab.create(this.noticia.url, '_system');
+  ngOnInit() {
+    // console.log("prueba");
   }
 
+  onClick() {
+    this.iab.create(this.noticia.url, "_system");
+    // this.iab.create(this.noticia.url);
+  }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: "Compartir",
+          icon: "share",
+          handler: () => {
+            console.log("Share clicked");
+          },
+        },
+        {
+          text: "Favorito",
+          icon: "heart-outline",
+          handler: () => {
+            console.log("Favorite clicked");
+          },
+        },
+        {
+          text: "Cancelar",
+          icon: "close",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel clicked");
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
 }
